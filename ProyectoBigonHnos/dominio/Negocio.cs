e8aeb5c73070bb9226b7e05d2be4774b85d82444
Daniel <danielyapura@gmail.com>
2018-09-24 17:18:50 -0300

    Se elimino codigo antiguo en catalogo y negocio

--- ProyectoBigonHnos/dominio/Negocio.cs
+++ ProyectoBigonHnos/dominio/Negocio.cs
@@ -5,27 +5,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using ProyectoBigonHnos.data;
-using ProyectoBigonHnos.data.ClienteDao;
-using ProyectoBigonHnos.data.EmpleadoDao;
-using ProyectoBigonHnos.data.VentaDao;
-using ProyectoBigonHnos.data.PedidoDao;
-using ProyectoBigonHnos.data.ConceptoDao;
-using ProyectoBigonHnos.data.MaterialDao;
 
 namespace ProyectoBigonHnos.dominio
 {
     class Negocio
     {
         private static Negocio negocio;
-
         private CatalogoDeMateriales catalogo;
-
         private PersistenciaFacade persistencia;
 
         public Negocio()
         {
             persistencia = PersistenciaFacade.getInstance();
-    }
+        }
 
         public static Negocio getNegocio()
         {
@@ -39,78 +31,60 @@ namespace ProyectoBigonHnos.dominio
             this.catalogo = catalogo;
         }
 
-       
-
         //CLIENTE
-
         internal IEnumerable<Cliente> obtenerTodosClientes()
-        {
-
-            //return clieteDao.listarTodos();
-
+        { 
             return persistencia.obtenerTodos<Cliente>();
         }
 
         public Cliente buscarCliente(string dni)
         {
             List<Cliente> listaClientes = persistencia.obtenerTodos<Cliente>();
-            //foreach( var unCliente in clieteDao.listarTodos())
+
             foreach (var unCliente in listaClientes)
             {
                 if (dni.Equals(unCliente.Dni))
                     return unCliente;
             }
-
             return null;
         }
 
         public void agregarCliente(Cliente cliente)
         {
-
-            //clieteDao.registrar(cliente);
             persistencia.registrarObjeto(cliente);
         }
 
         public void borrarCliente(string dni)
         {
-            //List<Cliente> listaDeClientes = clieteDao.listarTodos();
             List<Cliente> listaDeClientes = persistencia.obtenerTodos<Cliente>();
 
             for (int i = 0; i < listaDeClientes.Count; i++)
             {
                 if (listaDeClientes.ElementAt(i).Dni == dni)
-                    //clieteDao.eliminar(listaDeClientes.ElementAt(i).IdCliente);
-                    persistencia.eliminarObjeto<Cliente>( listaDeClientes.ElementAt(i).IdCliente);
-                    
+                    persistencia.eliminarObjeto<Cliente>(listaDeClientes.ElementAt(i).IdCliente);
             }
-
         }
 
 
         //MATERIAL
         public Material buscarMaterial(int idMaterial)
         {
-            return catalogo.obtenerMaterial(idMaterial);
+            return CatalogoDeMateriales.getInstancia().obtenerMaterial(idMaterial);
         }
-
 
         //VENTA
         internal IEnumerable<Venta> obtenerTodasVentasRealizadas()
         {
-            //return ventaDao.listarTodos();
             return persistencia.obtenerTodos<Venta>();
         }
 
         public void BorrarVenta(int idVenta)
         {
-            //ventaDao.eliminar(idVenta);
             persistencia.eliminarObjeto<Venta>(idVenta);
-
         }
 
         public void cargarVentasConfirmadas(Venta venta)
         {
-            //ventaDao.registrar(venta);
             persistencia.registrarObjeto(venta);
         }
 
@@ -118,13 +92,11 @@ namespace ProyectoBigonHnos.dominio
         //EMPLEADO
         public List<Empleado> obtenerTodosEmpleados()
         {
-            //return empleadoDao.listarTodos();
             return persistencia.obtenerTodos<Empleado>();
         }
 
         public void borrarEmpleado(string legajo)
         {
-            //List<Empleado> listaEmpleados = empleadoDao.listarTodos();
             List<Empleado> listaEmpleados = persistencia.obtenerTodos<Empleado>();
 
             for ( int i = 0; i < listaEmpleados.Count; i++)
@@ -136,14 +108,11 @@ namespace ProyectoBigonHnos.dominio
 
         internal Empleado buscarEmpleado(string legajoSeleccionado)
         {
-
-            //List<Empleado> listaEmpleados = empleadoDao.listarTodos();
             List<Empleado> listaEmpleados = persistencia.obtenerTodos<Empleado>();
 
             for (int i = 0; i < listaEmpleados.Count; i++)
             {
                 if (listaEmpleados[i].Legajo == legajoSeleccionado)
-                    //return empleadoDao.leerPorId(listaEmpleados[i].IdEmpleado);
                     return persistencia.obtenerObjeto<Empleado>(listaEmpleados[i].IdEmpleado);
             }
             return null;
@@ -156,71 +125,58 @@ namespace ProyectoBigonHnos.dominio
 
         public void agregarEmpleado(Empleado empleado)
         {
-            //empleadoDao.registrar(empleado);
             persistencia.registrarObjeto(empleado);
         }
-
 
         //PEDIDOS
         internal IEnumerable<Pedido> obtenerTodosPedidosRealizados()
         {
-            //return pedidoDao.listarTodos();
             return persistencia.obtenerTodos<Pedido>();
         }
 
         public Pedido buscarPedido(int idPedido)
         {
-            //return pedidoDao.leerPorId(idPedido);
             return persistencia.obtenerObjeto<Pedido>(idPedido);
         }
 
         internal IEnumerable<Pedido> obtenerTodosPedidos()
         {
-            //return pedidoDao.listarTodos();
             return persistencia.obtenerTodos<Pedido>();
         }
 
         public void cargarPedidosRealizados(Pedido pedido)
         {
-            //pedidoDao.registrar(pedido);
             persistencia.registrarObjeto(pedido);
         }
 
         public void borrarPedido(int idPedido)
         {
-            //pedidoDao.eliminar(idPedido);
             persistencia.eliminarObjeto<Pedido>(idPedido);
         }
-
 
         //CONCEPTOS
         internal IEnumerable<Concepto> obtenerTodosConceptos()
         {
-            //return conceptoDao.listarTodos();
             return persistencia.obtenerTodos<Concepto>();
         }
 
         public void agregarConcepto(Concepto concepto)
         {
-            //conceptoDao.registrar(concepto);
             persistencia.registrarObjeto(concepto);
         }
 
         public void borrarConcepto(int idConcepto)
         {
-            //conceptoDao.eliminar(idConcepto);
             persistencia.eliminarObjeto<Concepto>(idConcepto);
         }
 
         public Concepto buscarConcepto(int idConcepto)
         {
-            //return conceptoDao.leerPorId(idConcepto);
             return persistencia.obtenerObjeto<Concepto>(idConcepto);
         }
 
         internal void actualizarConcepto(Concepto concepto)
         {
-            //conceptoDao.actualizar(concepto);
             persistencia.actualiarObjeto(concepto);
         }
 
@@ -244,7 +200,6 @@ namespace ProyectoBigonHnos.dominio
 
         public Proveedor buscarProveedor(int idProveedor)
         {
-            //return proveedorDao.leerPorId(idProveedor);
             return persistencia.obtenerObjeto<Proveedor>(idProveedor);
         }
 
@@ -254,7 +209,6 @@ namespace ProyectoBigonHnos.dominio
             foreach (var unProveedor in listaProveedores)
             {
                 if (unProveedor.RazonSocial == razonSocial)
-                    //return proveedorDao.leerPorId(unProveedor.IdProveedor);
                     return persistencia.obtenerObjeto<Proveedor>(unProveedor.IdProveedor);
             }
 
@@ -263,24 +217,17 @@ namespace ProyectoBigonHnos.dominio
 
         public void actualizarProveedor(int idProveedor, Proveedor proveedor)
         {
-
-            //proveedorDao.actualizar(proveedor); 
             persistencia.actualiarObjeto(proveedor);
         }
 
         public void agregarProveedor(Proveedor proveedor)
         {
-            //proveedorDao.registrar(proveedor);
             persistencia.registrarObjeto(proveedor);
         }
 
         public void borrarProveedor(string razonSocial)
         {
             List<Proveedor> listaProveedores = persistencia.obtenerTodos<Proveedor>();
-            //foreach (var unProveedor in listaProveedores)
-            //    if (unProveedor.RazonSocial == razonSocial)
-            //        //proveedorDao.eliminar(unProveedor.IdProveedor);
-            //        persistencia.eliminarObjeto<Proveedor>(unProveedor.IdProveedor);
 
             for (int i = 0; i < listaProveedores.Count; i++)
             {
@@ -291,13 +238,11 @@ namespace ProyectoBigonHnos.dominio
 
         public void borrarProveedor(int idProveedor)
         {
-            //proveedorDao.eliminar(idProveedor);
             persistencia.eliminarObjeto<Proveedor>(idProveedor);
         }
 
         public List<Proveedor> obtenerTodosProveedores()
         {
-            //return proveedorDao.listarTodos();
             return persistencia.obtenerTodos<Proveedor>();
         }
     }
--- ProyectoBigonHnos/dominio/material/CatalogoDeMateriales.cs
+++ ProyectoBigonHnos/dominio/material/CatalogoDeMateriales.cs
@@ -10,22 +10,11 @@ namespace ProyectoBigonHnos.dominio
 {
     class CatalogoDeMateriales
     {
-        //public const int INDICE_NO_ENCONTRADO = -1;
-
         public static CatalogoDeMateriales catalogo;
-
-        //public List<Material> materiales { get; set; }
-
-        private CRUD<Material> dao { get; set; }
-
         private PersistenciaFacade persistencia;
 
         public CatalogoDeMateriales()
         {
-            //materiales = new List<Material>();
-
-            dao = new MaterialDaoListImpl();
-
             persistencia = PersistenciaFacade.getInstance();
         }
 
@@ -40,17 +29,6 @@ namespace ProyectoBigonHnos.dominio
 
         public Material obtenerMaterial(int idMaterial)
         {
-            //foreach (Material mat in materiales)
-            //{
-            //    if (idMaterial == mat.IdMaterial)
-            //    {
-            //        return mat;
-            //    }
-            //}
-            //return null;
-
-            //return dao.leerPorId(idMaterial);
-
             return persistencia.obtenerObjeto<Material>(idMaterial);
         }
 
@@ -64,62 +42,25 @@ namespace ProyectoBigonHnos.dominio
 
         public void crearMaterial(string descripcion, int cantidad, double precio, int stockDisponible, int stockMinimo)
         {
-            //materiales.Add(new Material(descripcion, cantidad,precio, stockDisponible, stockMinimo));
-            //dao.registrar(new Material(descripcion, cantidad, precio, stockDisponible, stockMinimo));
             persistencia.registrarObjeto(new Material(descripcion, cantidad, precio, stockDisponible, stockMinimo));
 
         }
 
         public void borrarMaterial(int idMaterial)
         {
-            //int indice = getIndiceBuscandoPorId(idMaterial);
-
-            //if (indice != INDICE_NO_ENCONTRADO)
-            //    materiales.RemoveAt(indice);
-
-            //dao.eliminar(idMaterial);
             persistencia.eliminarObjeto<Material>(idMaterial);
         }
 
-
         public void editarMaterial(int id, string descripcion, int cantidad, double precio, int stockMinimo, int stockDisponible)
         {
-
-            //int indice = getIndiceBuscandoPorId(id);
-
-            //if (indice != INDICE_NO_ENCONTRADO)
-            //{
-            //    materiales[indice].Descripcion = descripcion;
-            //    materiales[indice].Cantidad = cantidad;
-            //    materiales[indice].Precio = precio;
-            //    materiales[indice].StockMinimo = stockMinimo;
-            //    materiales[indice].StockDisponible = stockDisponible;
-            //}
-
             Material material = new Material(descripcion, cantidad, precio, stockDisponible, stockMinimo);
             material.IdMaterial = id;
-            //dao.actualizar(material);
             persistencia.actualiarObjeto(material);
         }
 
         public List<Material> obtenerMateriales()
         {
-            //return dao.listarTodos();
             return persistencia.obtenerTodos<Material>();
         }
-
-        //private int getIndiceBuscandoPorId(int idMaterial)
-        //{
-        //    for (int i = 0; i < materiales.Count; i++)
-        //    {
-        //        if (materiales.ElementAt(i).IdMaterial == idMaterial)
-        //        {
-        //            return i;
-        //        }
-        //    }
-
-        //    return INDICE_NO_ENCONTRADO;
-        //}
-
     }
 }
