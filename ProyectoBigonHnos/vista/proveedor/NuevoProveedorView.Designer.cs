9e25e1cadf3d45a7d181641d4ca9e098a77699f0
Daniel <danielyapura@gmail.com>
2019-02-20 17:55:02 -0300

    Se cambiaron algunas propiedades de la Nueov Proveedor view

--- ProyectoBigonHnos/vista/proveedor/NuevoProveedorView.Designer.cs
+++ ProyectoBigonHnos/vista/proveedor/NuevoProveedorView.Designer.cs
@@ -229,6 +229,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tboxRazonSocial);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "NuevoProveedorView";
