1761d4b7655afa0545d8af13c7274c638eb4c09e
Daniel <danielyapura@gmail.com>
2019-02-22 18:02:50 -0300

    Se modifico la interfaz de nuevo material NuevoMaterialView

--- ProyectoBigonHnos/vista/materiales/NuevoMaterialView.Designer.cs
+++ ProyectoBigonHnos/vista/materiales/NuevoMaterialView.Designer.cs
@@ -40,11 +40,14 @@
             this.tboxStockDisponible = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.tboxMinimo = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(656, 327);
+            this.button1.Location = new System.Drawing.Point(325, 338);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
@@ -54,17 +57,17 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(563, 327);
+            this.button2.Location = new System.Drawing.Point(232, 338);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
+            this.button2.TabIndex = 7;
             this.button2.Text = "Cancelar";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 67);
+            this.label1.Location = new System.Drawing.Point(115, 142);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 2;
@@ -72,15 +75,15 @@
             // 
             // tboxDescripcion
             // 
-            this.tboxDescripcion.Location = new System.Drawing.Point(87, 84);
+            this.tboxDescripcion.Location = new System.Drawing.Point(202, 139);
             this.tboxDescripcion.Name = "tboxDescripcion";
             this.tboxDescripcion.Size = new System.Drawing.Size(100, 20);
-            this.tboxDescripcion.TabIndex = 0;
+            this.tboxDescripcion.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(84, 111);
+            this.label2.Location = new System.Drawing.Point(141, 177);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(37, 13);
             this.label2.TabIndex = 2;
@@ -88,15 +91,15 @@
             // 
             // tboxPrecio
             // 
-            this.tboxPrecio.Location = new System.Drawing.Point(87, 128);
+            this.tboxPrecio.Location = new System.Drawing.Point(202, 174);
             this.tboxPrecio.Name = "tboxPrecio";
             this.tboxPrecio.Size = new System.Drawing.Size(100, 20);
-            this.tboxPrecio.TabIndex = 1;
+            this.tboxPrecio.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(84, 168);
+            this.label3.Location = new System.Drawing.Point(129, 212);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 13);
             this.label3.TabIndex = 2;
@@ -104,15 +107,15 @@
             // 
             // tboxCantidad
             // 
-            this.tboxCantidad.Location = new System.Drawing.Point(87, 185);
+            this.tboxCantidad.Location = new System.Drawing.Point(202, 209);
             this.tboxCantidad.Name = "tboxCantidad";
             this.tboxCantidad.Size = new System.Drawing.Size(100, 20);
-            this.tboxCantidad.TabIndex = 2;
+            this.tboxCantidad.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(84, 219);
+            this.label4.Location = new System.Drawing.Point(93, 249);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 13);
             this.label4.TabIndex = 2;
@@ -120,7 +123,7 @@
             // 
             // tboxStockDisponible
             // 
-            this.tboxStockDisponible.Location = new System.Drawing.Point(87, 236);
+            this.tboxStockDisponible.Location = new System.Drawing.Point(202, 246);
             this.tboxStockDisponible.Name = "tboxStockDisponible";
             this.tboxStockDisponible.Size = new System.Drawing.Size(100, 20);
             this.tboxStockDisponible.TabIndex = 4;
@@ -128,7 +131,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(84, 265);
+            this.label5.Location = new System.Drawing.Point(107, 285);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(71, 13);
             this.label5.TabIndex = 2;
@@ -136,16 +139,40 @@
             // 
             // tboxMinimo
             // 
-            this.tboxMinimo.Location = new System.Drawing.Point(87, 282);
+            this.tboxMinimo.Location = new System.Drawing.Point(202, 282);
             this.tboxMinimo.Name = "tboxMinimo";
             this.tboxMinimo.Size = new System.Drawing.Size(100, 20);
             this.tboxMinimo.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label8.Location = new System.Drawing.Point(24, 40);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(196, 31);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Nuevo Material";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 93);
+            this.panel1.TabIndex = 11;
             // 
             // NuevoMaterialView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(436, 384);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.tboxMinimo);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.tboxStockDisponible);
@@ -158,8 +185,14 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "NuevoMaterialView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "NuevoMaterialView";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -179,5 +212,7 @@
         private System.Windows.Forms.TextBox tboxStockDisponible;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox tboxMinimo;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
     }
 }
