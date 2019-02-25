120c71ad4482ff68ab1b238e6db9bd6d2dac0275
Daniel <danielyapura@gmail.com>
2019-02-25 17:39:06 -0300

    Se modifico la interfaz de agregar un nuevo concepto

--- ProyectoBigonHnos/vista/conceptos/ConceptosView.Designer.cs
+++ ProyectoBigonHnos/vista/conceptos/ConceptosView.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.conceptosDataGrid = new System.Windows.Forms.DataGridView();
             this.colIdConcepto = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -51,9 +52,9 @@
             this.tipoNuevoComboBox = new System.Windows.Forms.ComboBox();
             this.obligatorioNuevoComboBox = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.eliminarConceptoButton = new System.Windows.Forms.Button();
             this.obligatorioDetalleTextView = new System.Windows.Forms.ComboBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.eliminarConceptoButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.conceptosDataGrid)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -62,9 +63,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.Size = new System.Drawing.Size(127, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Conceptos Agregados";
             // 
@@ -72,20 +74,36 @@
             // 
             this.conceptosDataGrid.AllowUserToAddRows = false;
             this.conceptosDataGrid.AllowUserToDeleteRows = false;
+            this.conceptosDataGrid.AllowUserToResizeColumns = false;
+            this.conceptosDataGrid.AllowUserToResizeRows = false;
             this.conceptosDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.conceptosDataGrid.BackgroundColor = System.Drawing.SystemColors.ControlLight;
-            this.conceptosDataGrid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.conceptosDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.conceptosDataGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.conceptosDataGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.conceptosDataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.conceptosDataGrid.ColumnHeadersHeight = 40;
+            this.conceptosDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.conceptosDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colIdConcepto,
             this.colDescripcionConcepto});
-            this.conceptosDataGrid.GridColor = System.Drawing.SystemColors.InactiveBorder;
-            this.conceptosDataGrid.Location = new System.Drawing.Point(26, 56);
+            this.conceptosDataGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.conceptosDataGrid.Location = new System.Drawing.Point(26, 50);
             this.conceptosDataGrid.Name = "conceptosDataGrid";
             this.conceptosDataGrid.ReadOnly = true;
+            this.conceptosDataGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.conceptosDataGrid.RowHeadersVisible = false;
             this.conceptosDataGrid.RowHeadersWidth = 25;
+            this.conceptosDataGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.conceptosDataGrid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.conceptosDataGrid.Size = new System.Drawing.Size(221, 323);
+            this.conceptosDataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.conceptosDataGrid.Size = new System.Drawing.Size(221, 350);
             this.conceptosDataGrid.TabIndex = 1;
             this.conceptosDataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.seleccionarFila);
             // 
@@ -104,7 +122,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 30);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 27);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(64, 13);
             this.label3.TabIndex = 0;
@@ -113,7 +132,8 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(169, 30);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(169, 27);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(63, 13);
             this.label4.TabIndex = 0;
@@ -122,7 +142,8 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(20, 82);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 85);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(31, 13);
             this.label5.TabIndex = 0;
@@ -131,7 +152,8 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(169, 82);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(169, 85);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(58, 13);
             this.label6.TabIndex = 0;
@@ -140,7 +162,8 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(327, 82);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(327, 85);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(57, 13);
             this.label7.TabIndex = 0;
@@ -151,7 +174,7 @@
             this.idConceptoDetalleTextView.Location = new System.Drawing.Point(23, 44);
             this.idConceptoDetalleTextView.Name = "idConceptoDetalleTextView";
             this.idConceptoDetalleTextView.ReadOnly = true;
-            this.idConceptoDetalleTextView.Size = new System.Drawing.Size(100, 20);
+            this.idConceptoDetalleTextView.Size = new System.Drawing.Size(100, 21);
             this.idConceptoDetalleTextView.TabIndex = 2;
             // 
             // descripcionDetalleTextView
@@ -159,28 +182,29 @@
             this.descripcionDetalleTextView.Location = new System.Drawing.Point(172, 44);
             this.descripcionDetalleTextView.Name = "descripcionDetalleTextView";
             this.descripcionDetalleTextView.ReadOnly = true;
-            this.descripcionDetalleTextView.Size = new System.Drawing.Size(258, 20);
+            this.descripcionDetalleTextView.Size = new System.Drawing.Size(258, 21);
             this.descripcionDetalleTextView.TabIndex = 2;
             // 
             // tipoDetalleTextView
             // 
-            this.tipoDetalleTextView.Location = new System.Drawing.Point(23, 96);
+            this.tipoDetalleTextView.Location = new System.Drawing.Point(23, 102);
             this.tipoDetalleTextView.Name = "tipoDetalleTextView";
             this.tipoDetalleTextView.ReadOnly = true;
-            this.tipoDetalleTextView.Size = new System.Drawing.Size(100, 20);
+            this.tipoDetalleTextView.Size = new System.Drawing.Size(100, 21);
             this.tipoDetalleTextView.TabIndex = 2;
             // 
             // porcentajeDetalleTextView
             // 
-            this.porcentajeDetalleTextView.Location = new System.Drawing.Point(172, 96);
+            this.porcentajeDetalleTextView.Location = new System.Drawing.Point(172, 102);
             this.porcentajeDetalleTextView.Name = "porcentajeDetalleTextView";
             this.porcentajeDetalleTextView.ReadOnly = true;
-            this.porcentajeDetalleTextView.Size = new System.Drawing.Size(100, 20);
+            this.porcentajeDetalleTextView.Size = new System.Drawing.Size(100, 21);
             this.porcentajeDetalleTextView.TabIndex = 2;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.Location = new System.Drawing.Point(20, 29);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(63, 13);
@@ -190,6 +214,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.Location = new System.Drawing.Point(20, 84);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(31, 13);
@@ -199,6 +224,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.Location = new System.Drawing.Point(169, 84);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(58, 13);
@@ -208,6 +234,7 @@
             // label13
             // 
             this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.Location = new System.Drawing.Point(327, 84);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(57, 13);
@@ -218,21 +245,22 @@
             // 
             this.descripcionNuevoTextView.Location = new System.Drawing.Point(23, 46);
             this.descripcionNuevoTextView.Name = "descripcionNuevoTextView";
-            this.descripcionNuevoTextView.Size = new System.Drawing.Size(258, 20);
+            this.descripcionNuevoTextView.Size = new System.Drawing.Size(258, 21);
             this.descripcionNuevoTextView.TabIndex = 2;
             // 
             // porcentajeNuevoTextView
             // 
             this.porcentajeNuevoTextView.Location = new System.Drawing.Point(172, 100);
             this.porcentajeNuevoTextView.Name = "porcentajeNuevoTextView";
-            this.porcentajeNuevoTextView.Size = new System.Drawing.Size(100, 20);
+            this.porcentajeNuevoTextView.Size = new System.Drawing.Size(100, 21);
             this.porcentajeNuevoTextView.TabIndex = 2;
             // 
             // agregarButton
             // 
+            this.agregarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.agregarButton.Location = new System.Drawing.Point(355, 140);
             this.agregarButton.Name = "agregarButton";
-            this.agregarButton.Size = new System.Drawing.Size(75, 23);
+            this.agregarButton.Size = new System.Drawing.Size(75, 30);
             this.agregarButton.TabIndex = 3;
             this.agregarButton.Text = "Agregar";
             this.agregarButton.UseVisualStyleBackColor = true;
@@ -243,14 +271,14 @@
             this.tipoNuevoComboBox.FormattingEnabled = true;
             this.tipoNuevoComboBox.Location = new System.Drawing.Point(23, 100);
             this.tipoNuevoComboBox.Name = "tipoNuevoComboBox";
-            this.tipoNuevoComboBox.Size = new System.Drawing.Size(100, 21);
+            this.tipoNuevoComboBox.Size = new System.Drawing.Size(100, 23);
             this.tipoNuevoComboBox.TabIndex = 4;
             // 
             // obligatorioNuevoComboBox
             // 
             this.obligatorioNuevoComboBox.Location = new System.Drawing.Point(330, 100);
             this.obligatorioNuevoComboBox.Name = "obligatorioNuevoComboBox";
-            this.obligatorioNuevoComboBox.Size = new System.Drawing.Size(100, 21);
+            this.obligatorioNuevoComboBox.Size = new System.Drawing.Size(100, 23);
             this.obligatorioNuevoComboBox.TabIndex = 4;
             // 
             // groupBox1
@@ -266,20 +294,32 @@
             this.groupBox1.Controls.Add(this.idConceptoDetalleTextView);
             this.groupBox1.Controls.Add(this.tipoDetalleTextView);
             this.groupBox1.Controls.Add(this.descripcionDetalleTextView);
-            this.groupBox1.Location = new System.Drawing.Point(283, 40);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(283, 21);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(450, 167);
+            this.groupBox1.Size = new System.Drawing.Size(450, 179);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Detalle de concepto";
+            // 
+            // eliminarConceptoButton
+            // 
+            this.eliminarConceptoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eliminarConceptoButton.Location = new System.Drawing.Point(355, 135);
+            this.eliminarConceptoButton.Name = "eliminarConceptoButton";
+            this.eliminarConceptoButton.Size = new System.Drawing.Size(75, 30);
+            this.eliminarConceptoButton.TabIndex = 5;
+            this.eliminarConceptoButton.Text = "Eliminar";
+            this.eliminarConceptoButton.UseVisualStyleBackColor = true;
+            this.eliminarConceptoButton.Click += new System.EventHandler(this.eliminarConceptoButton_Click);
             // 
             // obligatorioDetalleTextView
             // 
             this.obligatorioDetalleTextView.Enabled = false;
             this.obligatorioDetalleTextView.FormattingEnabled = true;
-            this.obligatorioDetalleTextView.Location = new System.Drawing.Point(330, 96);
+            this.obligatorioDetalleTextView.Location = new System.Drawing.Point(330, 101);
             this.obligatorioDetalleTextView.Name = "obligatorioDetalleTextView";
-            this.obligatorioDetalleTextView.Size = new System.Drawing.Size(100, 21);
+            this.obligatorioDetalleTextView.Size = new System.Drawing.Size(100, 23);
             this.obligatorioDetalleTextView.TabIndex = 4;
             // 
             // groupBox2
@@ -293,27 +333,19 @@
             this.groupBox2.Controls.Add(this.porcentajeNuevoTextView);
             this.groupBox2.Controls.Add(this.label12);
             this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(283, 225);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(450, 181);
+            this.groupBox2.Size = new System.Drawing.Size(450, 188);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Nuevo concepto";
-            // 
-            // eliminarConceptoButton
-            // 
-            this.eliminarConceptoButton.Location = new System.Drawing.Point(355, 131);
-            this.eliminarConceptoButton.Name = "eliminarConceptoButton";
-            this.eliminarConceptoButton.Size = new System.Drawing.Size(75, 23);
-            this.eliminarConceptoButton.TabIndex = 5;
-            this.eliminarConceptoButton.Text = "Eliminar";
-            this.eliminarConceptoButton.UseVisualStyleBackColor = true;
-            this.eliminarConceptoButton.Click += new System.EventHandler(this.eliminarConceptoButton_Click);
             // 
             // ConceptosView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(749, 439);
             this.Controls.Add(this.conceptosDataGrid);
             this.Controls.Add(this.label1);
