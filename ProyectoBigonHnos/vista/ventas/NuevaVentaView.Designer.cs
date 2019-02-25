26a7e8af1430db7989b69801c1c2858f94cb891a
Daniel <danielyapura@gmail.com>
2019-02-25 16:08:35 -0300

    Se modifico la interfaz de agregar nueva venta

--- ProyectoBigonHnos/vista/ventas/NuevaVentaView.Designer.cs
+++ ProyectoBigonHnos/vista/ventas/NuevaVentaView.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -38,13 +39,15 @@
             this.colCliente = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colFechaPedido = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnDetalles = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPedidos)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(330, 392);
+            this.btnGuardar.Location = new System.Drawing.Point(329, 454);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(75, 23);
             this.btnGuardar.TabIndex = 0;
@@ -54,7 +57,7 @@
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(209, 392);
+            this.btnCancelar.Location = new System.Drawing.Point(236, 454);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(75, 23);
             this.btnCancelar.TabIndex = 0;
@@ -64,22 +67,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 60);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 122);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.Size = new System.Drawing.Size(82, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Dni de cliente";
             // 
             // tboxDni
             // 
-            this.tboxDni.Location = new System.Drawing.Point(104, 57);
+            this.tboxDni.Location = new System.Drawing.Point(113, 121);
             this.tboxDni.Name = "tboxDni";
             this.tboxDni.Size = new System.Drawing.Size(208, 20);
             this.tboxDni.TabIndex = 2;
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(330, 57);
+            this.btnBuscar.Location = new System.Drawing.Point(329, 119);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(75, 23);
             this.btnBuscar.TabIndex = 3;
@@ -89,13 +93,45 @@
             // 
             // dgvPedidos
             // 
-            this.dgvPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPedidos.AllowUserToAddRows = false;
+            this.dgvPedidos.AllowUserToDeleteRows = false;
+            this.dgvPedidos.AllowUserToResizeColumns = false;
+            this.dgvPedidos.AllowUserToResizeRows = false;
+            this.dgvPedidos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvPedidos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPedidos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dgvPedidos.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvPedidos.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+            this.dgvPedidos.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPedidos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvPedidos.ColumnHeadersHeight = 40;
+            this.dgvPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.dgvPedidos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colIdPedido,
             this.colCliente,
             this.colFechaPedido});
-            this.dgvPedidos.Location = new System.Drawing.Point(29, 104);
+            this.dgvPedidos.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dgvPedidos.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.dgvPedidos.Location = new System.Drawing.Point(28, 171);
             this.dgvPedidos.Name = "dgvPedidos";
+            this.dgvPedidos.ReadOnly = true;
+            this.dgvPedidos.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvPedidos.RowHeadersVisible = false;
+            this.dgvPedidos.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvPedidos.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvPedidos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPedidos.ShowCellErrors = false;
+            this.dgvPedidos.ShowCellToolTips = false;
+            this.dgvPedidos.ShowEditingIcon = false;
+            this.dgvPedidos.ShowRowErrors = false;
             this.dgvPedidos.Size = new System.Drawing.Size(376, 213);
             this.dgvPedidos.TabIndex = 4;
             // 
@@ -103,44 +139,60 @@
             // 
             this.colIdPedido.HeaderText = "Id Pedido";
             this.colIdPedido.Name = "colIdPedido";
+            this.colIdPedido.ReadOnly = true;
             // 
             // colCliente
             // 
             this.colCliente.HeaderText = "Cliente";
             this.colCliente.Name = "colCliente";
+            this.colCliente.ReadOnly = true;
             // 
             // colFechaPedido
             // 
             this.colFechaPedido.HeaderText = "Fecha de Pedido";
             this.colFechaPedido.Name = "colFechaPedido";
+            this.colFechaPedido.ReadOnly = true;
             // 
             // btnDetalles
             // 
-            this.btnDetalles.Location = new System.Drawing.Point(29, 323);
+            this.btnDetalles.Location = new System.Drawing.Point(329, 401);
             this.btnDetalles.Name = "btnDetalles";
             this.btnDetalles.Size = new System.Drawing.Size(75, 23);
             this.btnDetalles.TabIndex = 0;
             this.btnDetalles.Text = "Detalle";
             this.btnDetalles.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // panel1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 25);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "NUEVA VENTA";
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 93);
+            this.panel1.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label8.Location = new System.Drawing.Point(24, 40);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(171, 31);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Nueva Venta";
             // 
             // NuevaVentaView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(436, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(436, 500);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.dgvPedidos);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.tboxDni);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnDetalles);
             this.Controls.Add(this.btnCancelar);
@@ -148,6 +200,8 @@
             this.Name = "NuevaVentaView";
             this.Text = "NuevaVentaView";
             ((System.ComponentModel.ISupportInitialize)(this.dgvPedidos)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,6 +219,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colCliente;
         private System.Windows.Forms.DataGridViewTextBoxColumn colFechaPedido;
         private System.Windows.Forms.Button btnDetalles;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
     }
 }
