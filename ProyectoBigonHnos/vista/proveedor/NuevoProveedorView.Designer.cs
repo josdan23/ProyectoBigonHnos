238c6cc0aa037afc009e6ee341d7396f7966b0eb
Daniel <danielyapura@gmail.com>
2019-04-20 11:43:00 -0300

    Se modifico las interfaz de Editar y nuevo proveedor

--- ProyectoBigonHnos/vista/proveedor/NuevoProveedorView.Designer.cs
+++ ProyectoBigonHnos/vista/proveedor/NuevoProveedorView.Designer.cs
@@ -186,7 +186,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label8.ForeColor = System.Drawing.Color.Navy;
             this.label8.Location = new System.Drawing.Point(24, 40);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(225, 31);
